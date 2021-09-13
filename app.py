a49902153ca4b73277f68f77eca16f0879a38ffa
Youplala <eliebrosset@gmail.com>
2021-09-13 16:25:11 +0200

    error message

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import requests
 
+# Load HuggingFace API
+
 API_URL = "https://api-inference.huggingface.co/models/bhadresh-savani/distilbert-base-uncased-emotion"
 headers = {"Authorization": "Bearer api_OKvBwwcmNPTBaVNmvzmfSrWAmvpJZshtJE"}
 
@@ -10,20 +12,26 @@ def query(payload):
 	return response.json()
 
 
+# Display form
+
 st.title('Text emotion detector !')
 
 form = st.form(key='my_form')
 text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
 
-if submit_button:
-    output = query({"inputs": text})[0]
-    st.subheader('Data')
-    li = []
-    for out in output:
-        li.append([out["label"], out["score"]])
+# 
 
-    df = pd.DataFrame(li)
-    df.columns = ["Emotion", "Score"]
-    df = df.sort_values(ascending=False, by="Score")
-    st.table(df.assign(hack='').set_index('hack'))
+if submit_button:
+    try:
+        output = query({"inputs": text})[0]
+        st.subheader('Data')
+        li = []
+        for out in output:
+            li.append([out["label"], out["score"]])
+        df = pd.DataFrame(li)
+        df.columns = ["Emotion", "Score"]
+        df = df.sort_values(ascending=False, by="Score")
+        st.table(df.assign(hack='').set_index('hack'))
+    except:
+        st.write("Model is loading, please try again later")
