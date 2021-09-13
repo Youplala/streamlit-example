4205a91d0b20a76f872b9ecd62416086b72c2726
Youplala <eliebrosset@gmail.com>
2021-09-13 16:04:40 +0200

    style

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/bhadresh-savani/distilbert-base-uncased-emotion"
@@ -17,6 +17,12 @@ text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
 
 if submit_button:
-    output = query({"inputs": text})
+    output = query({"inputs": text})[0]
     st.subheader('Data')
-    st.json({"text": output})
+    li = []
+    for out in output:
+        li.append([out["label"], out["score"]])
+
+    df = pd.DataFrame(li)
+    df.columns = ["Emotion", "Score"]
+    st.table(df.assign(hack='').set_index('hack'))
